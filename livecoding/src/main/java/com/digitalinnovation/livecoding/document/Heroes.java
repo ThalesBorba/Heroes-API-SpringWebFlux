8ce3187818f611693ac703748d52a932a99912f8
ThalesBorba <thales_borba@hotmail.com>
2021-12-08 22:31:33 -0300

    Primeiro commit

---
+++ livecoding/src/main/java/com/digitalinnovation/livecoding/document/Heroes.java
@@ -0,0 +1,36 @@
+package com.digitalinnovation.livecoding.document;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import org.springframework.data.annotation.Id;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamoDBTable(tableName ="Heroes_Api_Table")
+
+
+public class Heroes {
+
+  @Id
+  @DynamoDBHashKey (attributeName = "id")
+  private String id;
+
+  @DynamoDBAttribute (attributeName = "name")
+  private String name;
+
+  @DynamoDBAttribute (attributeName = "universe")
+  private String universe;
+
+  @DynamoDBAttribute (attributeName = "films")
+  private int films;
+
+
+}
+
